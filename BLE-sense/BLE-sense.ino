7fdd75e555d54fde67c7889c4c38c5fd03701af5
Maria Paula Saba <maria.saba@alice-app.com>
2019-09-12 01:28:30 -0400

    Pressure, Humidity Sensors completed. Accelerometer/Gyro/Mag/Gestures in Progress.

--- BLE-sense/BLE-sense.ino
+++ BLE-sense/BLE-sense.ino
@@ -1,3 +1,4 @@
+
 /*
  * This example shows how to read sensor data from the Nano BLE Sense 
  * and to store in a service that can be read by central devices.
@@ -11,6 +12,8 @@
 #include <ArduinoBLE.h>
 #include <Arduino_APDS9960.h>
 #include <Arduino_HTS221.h>
+#include <Arduino_LPS22HB.h>
+#include <Arduino_LSM9DS1.h>
 
 
 BLEService nanoService("19b10010-e8f2-537e-4f6c-d104768a1214"); // BLE Nano Service
@@ -19,6 +22,14 @@ BLEByteCharacteristic tempCharacteristic("19b10012-e8f2-537e-4f6c-d104768a1214",
 BLEByteCharacteristic redCharacteristic("19b10013-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
 BLEByteCharacteristic greenCharacteristic("19b10014-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
 BLEByteCharacteristic blueCharacteristic("19b10015-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+BLEByteCharacteristic humidCharacteristic("19b10016-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+BLEByteCharacteristic pressCharacteristic("19b10017-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+BLEFloatCharacteristic accelXCharacteristic("19b10018-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+BLEFloatCharacteristic accelYCharacteristic("19b10019-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+BLEFloatCharacteristic accelZCharacteristic("19b10020-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+//BLEByteCharacteristic gyroCharacteristic("19b10019-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+//BLEByteCharacteristic magneCharacteristic("19b10020-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
+//BLEByteCharacteristic gestureCharacteristic("19b10021-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
 
 void setup() {
   //starts Serial
@@ -32,6 +43,11 @@ void setup() {
   
   if (!HTS.begin()) {
     Serial.println("Failed to initialize pressure sensor!");
+  }
+  
+ if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
   }
   
   if (!BLE.begin()) {
@@ -48,6 +64,12 @@ void setup() {
   nanoService.addCharacteristic(redCharacteristic);
   nanoService.addCharacteristic(greenCharacteristic);
   nanoService.addCharacteristic(blueCharacteristic);
+  nanoService.addCharacteristic(humidCharacteristic);
+  nanoService.addCharacteristic(pressCharacteristic);
+  nanoService.addCharacteristic(accelXCharacteristic);
+  nanoService.addCharacteristic(accelYCharacteristic);
+  nanoService.addCharacteristic(accelZCharacteristic);
+
   BLE.addService(nanoService);
 
   
@@ -70,11 +92,12 @@ void loop() {
         if (APDS.proximityAvailable()) {
           int proxValue = APDS.readProximity();
           proxCharacteristic.writeValue(proxValue);
+          Serial.println("proximity");
        }
 
 
       //Color Sensor
-        if (APDS.colorAvailable()) {
+        if (APDS.colorAvailable()) {          
           int r, g, b;
           APDS.readColor(r, g, b);
           r = map(r, 0, 4097, 0, 255);
@@ -86,12 +109,74 @@ void loop() {
         }
 
 
+       //Gesture
+//       if (APDS.gestureAvailable()) {
+//          // a gesture was detected
+//          int gestureValue = APDS.readGesture();
+//          Serial.println(gestureValue);
+//          gestureCharacteristic.writeValue(gestureValue);
+//        }
+
 
       //Temperature sensor
        float tempValue = HTS.readTemperature(FAHRENHEIT);
        tempCharacteristic.writeValue(tempValue);
+       
+     //Humidity  
+       float humidityValue = HTS.readHumidity();
+       humidCharacteristic.writeValue(humidityValue);
+
+      //Pressure  
+      float pressureValue = BARO.readPressure(PSI);
+      pressCharacteristic.writeValue(pressureValue);
+
+       //Accelerometer
+       
+
+        if (IMU.accelerationAvailable()) {
+          float x, y, z;
+          IMU.readAcceleration(x, y, z);
+          accelXCharacteristic.writeValue(x);
+          accelYCharacteristic.writeValue(y);
+          accelZCharacteristic.writeValue(z);
+          Serial.println("Accelerometer");
+          Serial.print(x);
+          Serial.print('\t');
+          Serial.print(y);
+          Serial.print('\t');
+          Serial.println(z);
+        }
+      
+      //Gyroscope
+        if (IMU.gyroscopeAvailable()) {
+           float x, y, z;
+          IMU.readGyroscope(x, y, z);
+//      
+//          Serial.println("Gyroscope");
+//          Serial.print(x);
+//          Serial.print('\t');
+//          Serial.print(y);
+//          Serial.print('\t');
+//          Serial.println(z);
+        }
+
+      
+      //Magnetometer
+      if (IMU.magneticFieldAvailable()) {
+            float x, y, z;
+            IMU.readMagneticField(x, y, z);
+//             Serial.println("Magnetometer");
+//     
+//            Serial.print(x);
+//            Serial.print('\t');
+//            Serial.print(y);
+//            Serial.print('\t');
+//            Serial.println(z);
+          }
     
-    delay(1000);
+
+    
+    delay(300);
 
     }
   }
--- Dashboard/sketch.js
+++ Dashboard/sketch.js
@@ -22,10 +22,11 @@ let redColor;
 let greenColor;
 let blueColor;
 
-// let light;
-// let pressure;
-// let humidity;
-// let accelerometer;
+let humidity;
+let pressure;
+let accelX;
+let accelY;
+let accelZ;
 // let gyroscope;
 // let rssi;
 
@@ -66,7 +67,11 @@ function receivedCharacteristics(error, characteristics) {
   
   for (let i = 0; i < characteristics.length; i++) {
       myBLE.startNotifications(characteristics[i], function(value){
-        handleSensorData(value, i);
+        if(i == 7){
+            handleSensorData(value, i, 'float32');
+        }
+        else handleSensorData(value, i);
+          
       });
     }    
 }
@@ -78,26 +83,65 @@ function handleSensorData(value, index) {
     temperature = senseValues[1];
     redColor  = senseValues[2];
     greenColor  = senseValues[3];   
-    blueColor  = senseValues[4];   
+    blueColor  = senseValues[4];  
+    humidity = senseValues[5];
+    pressure = senseValues[6];
+    accelX = senseValues[7];
+    accelY = senseValues[8];
+    accelZ = senseValues[9];
 }
 
 function draw() {
-   background(0,0,0);
+   background(50);
 
-   if(isConnected){
+   if(isConnected){    
+       
+       
        textSize(16);
        fill(255);
        text('Color Sensor', 20, 40);
+       text('Proximity: '+proximity, 180, 40);
+       text('Pressure: '+pressure, 180, 80);
+       text('Humidity: '+humidity+'%', 420, 40);
+       text('Temperature: '+temperature+'F', 420, 80);
+       text('Accelerometer: '+accelX+", "+accelY+", "+accelZ, 640, 40);
+//       switch(gesture){
+//               
+//         case '0':
+//         text('Gesture Detected: UP', 640, 80); 
+//         break;
+//            
+//         case '1':
+//         text('Gesture Detected: DOWN', 640, 80); 
+//         break;
+//            
+//         case '2':
+//         text('Gesture Detected: LEFT', 640, 80); 
+//         break;
+//               
+//         case '3':
+//         text('Gesture Detected: RIGHT', 640, 80); 
+//         break;
+//                                    
+//          default:
+//          //text('No Gesture Detected', 640, 80); 
+//          break;     
+//       }
 
        fill(redColor, greenColor, blueColor);
        rect(20,50, 120, 120);
+       
+       
+       
    } 
     
+    
+    
+    
    else{    
-
        textSize(16);
        fill(255);
-       text('Waiting to connect', 20, 40);
+       text('Waiting to connect...', 20, 40);
    }
     
     
